89a6979a3347896bfae150a865a991de9f3f44bd
karooolis <hello@karolisram.com>
2024-03-16 21:52:41 +0000

    connect autonomous world

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,94 +1,53 @@
-// import { useComponentValue } from "@latticexyz/react";
-// import { useMUD } from "./MUDContext";
-// import { singletonEntity } from "@latticexyz/store-sync/recs";
+import React from "react";
+import styled from "styled-components";
 
-// export const App = () => {
-//   const {
-//     components: { Counter },
-//     systemCalls: { increment },
-//   } = useMUD();
+import { useAppSelector } from "./hooks";
 
-//   const counter = useComponentValue(Counter, singletonEntity);
+import RoomSelectionDialog from "./components/RoomSelectionDialog";
+import LoginDialog from "./components/LoginDialog";
+import ComputerDialog from "./components/ComputerDialog";
+import WhiteboardDialog from "./components/WhiteboardDialog";
+import VideoConnectionDialog from "./components/VideoConnectionDialog";
+import Chat from "./components/Chat";
+import HelperButtonGroup from "./components/HelperButtonGroup";
+import MobileVirtualJoystick from "./components/MobileVirtualJoystick";
+import { useComponentValue } from "@latticexyz/react";
+import { singletonEntity } from "@latticexyz/store-sync/recs";
 
-//   return (
-//     <>
-//       <div>
-//         Counter: <span>{counter?.value ?? "??"}</span>
-//       </div>
-//       <button
-//         type="button"
-//         onClick={async (event) => {
-//           event.preventDefault();
-//           console.log("new counter value:", await increment());
-//         }}
-//       >
-//         Increment
-//       </button>
-//     </>
-//   );
-// };
-
-// import {
-//   DynamicContextProvider,
-//   DynamicWidget,
-// } from "@dynamic-labs/sdk-react-core";
-// import { EthersExtension } from "@dynamic-labs/ethers-v5";
-
-// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-
-// export const App = () => {
-//   return (
-//     <DynamicContextProvider
-//       settings={{
-//         // Find your environment id at https://app.dynamic.xyz/dashboard/developer
-//         environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-//         walletConnectorExtensions: [EthersExtension],
-//         walletConnectors: [EthereumWalletConnectors],
-//       }}
-//     >
-//       <DynamicWidget />
-//     </DynamicContextProvider>
-//   );
-// };
-
-// export default App;
-
-
-import React from 'react'
-import styled from 'styled-components'
-
-import { useAppSelector } from './hooks'
-
-import RoomSelectionDialog from './components/RoomSelectionDialog'
-import LoginDialog from './components/LoginDialog'
-import ComputerDialog from './components/ComputerDialog'
-import WhiteboardDialog from './components/WhiteboardDialog'
-import VideoConnectionDialog from './components/VideoConnectionDialog'
-import Chat from './components/Chat'
-import HelperButtonGroup from './components/HelperButtonGroup'
-import MobileVirtualJoystick from './components/MobileVirtualJoystick'
+import { useMUD } from "./MUDContext";
 
 const Backdrop = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
-`
+`;
 
 function App() {
-  const loggedIn = useAppSelector((state) => state.user.loggedIn)
-  const computerDialogOpen = useAppSelector((state) => state.computer.computerDialogOpen)
-  const whiteboardDialogOpen = useAppSelector((state) => state.whiteboard.whiteboardDialogOpen)
-  const videoConnected = useAppSelector((state) => state.user.videoConnected)
-  const roomJoined = useAppSelector((state) => state.room.roomJoined)
+  const {
+    components: { Counter },
+    systemCalls: { increment },
+  } = useMUD();
 
-  let ui: JSX.Element
+  const counter = useComponentValue(Counter, singletonEntity);
+
+  const loggedIn = useAppSelector((state) => state.user.loggedIn);
+  const computerDialogOpen = useAppSelector(
+    (state) => state.computer.computerDialogOpen
+  );
+  const whiteboardDialogOpen = useAppSelector(
+    (state) => state.whiteboard.whiteboardDialogOpen
+  );
+  const videoConnected = useAppSelector((state) => state.user.videoConnected);
+  const roomJoined = useAppSelector((state) => state.room.roomJoined);
+
+  let ui: JSX.Element;
   if (loggedIn) {
     if (computerDialogOpen) {
       /* Render ComputerDialog if user is using a computer. */
-      ui = <ComputerDialog />
+      ui = <ComputerDialog />;
     } else if (whiteboardDialogOpen) {
       /* Render WhiteboardDialog if user is using a whiteboard. */
-      ui = <WhiteboardDialog />
+      ui = <WhiteboardDialog />;
     } else {
       ui = (
         /* Render Chat or VideoConnectionDialog if no dialogs are opened. */
@@ -98,23 +57,44 @@ function App() {
           {!videoConnected && <VideoConnectionDialog />}
           <MobileVirtualJoystick />
         </>
-      )
+      );
     }
   } else if (roomJoined) {
     /* Render LoginDialog if not logged in but selected a room. */
-    ui = <LoginDialog />
+    ui = <LoginDialog />;
   } else {
     /* Render RoomSelectionDialog if yet selected a room. */
-    ui = <RoomSelectionDialog />
+    ui = <RoomSelectionDialog />;
   }
 
   return (
     <Backdrop>
+      <div
+        style={{
+          position: "fixed",
+          top: 15,
+          left: 15,
+        }}
+      >
+        <div>
+          Counter: <span>{counter?.value ?? "??"}</span>
+        </div>
+        <button
+          type="button"
+          onClick={async (event) => {
+            event.preventDefault();
+            console.log("new counter value:", await increment());
+          }}
+        >
+          Increment
+        </button>
+      </div>
+
       {ui}
       {/* Render HelperButtonGroup if no dialogs are opened. */}
       {/* {!computerDialogOpen && !whiteboardDialogOpen && <HelperButtonGroup />} */}
     </Backdrop>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,42 +1,56 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { ThemeProvider } from '@mui/material/styles'
+import "regenerator-runtime/runtime";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
 
-import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core'
-import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
-import './index.scss'
-import './PhaserGame'
-import muiTheme from './MuiTheme'
-import App from './App'
-import store from './stores'
+import "./index.scss";
+import "./PhaserGame";
+import muiTheme from "./MuiTheme";
+import App from "./App";
+import store from "./stores";
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <DynamicContextProvider
-        settings={{
-          // Find your environment id at https://app.dynamic.xyz/dashboard/developer
-          environmentId: '2762a57b-faa4-41ce-9f16-abff9300e2c9',
-          walletConnectors: [EthereumWalletConnectors],
-        }}
-      >
-        <div style={{
-          position: 'fixed',
-          top: 15,
-          right: 15,
-        }}>
-          <DynamicWidget />
-        </div>
+import { setup } from "./mud/setup";
+import { MUDProvider } from "./MUDContext";
+import mudConfig from "contracts/mud.config";
 
-        <ThemeProvider theme={muiTheme}>
-          <App />
-        </ThemeProvider>
-      </DynamicContextProvider>
-    </Provider>
-  </React.StrictMode>
-)
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+setup().then(async (result) => {
+  root.render(
+    <React.StrictMode>
+      <MUDProvider value={result}>
+        <Provider store={store}>
+          <DynamicContextProvider
+            settings={{
+              // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+              environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+              walletConnectors: [EthereumWalletConnectors],
+            }}
+          >
+            {/* <div
+              style={{
+                position: "fixed",
+                top: 15,
+                right: 15,
+              }}
+            >
+              <DynamicWidget />
+            </div> */}
+
+            <ThemeProvider theme={muiTheme}>
+              <App />
+            </ThemeProvider>
+          </DynamicContextProvider>
+        </Provider>
+      </MUDProvider>
+    </React.StrictMode>
+  );
+});
