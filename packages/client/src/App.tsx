ef5a0df17185d21adb4304f32edcba2d4d7385f7
karooolis <hello@karolisram.com>
2024-03-16 19:39:33 +0000

    add phaser opwn world

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -28,27 +28,93 @@
 //   );
 // };
 
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
-import { EthersExtension } from "@dynamic-labs/ethers-v5";
+// import {
+//   DynamicContextProvider,
+//   DynamicWidget,
+// } from "@dynamic-labs/sdk-react-core";
+// import { EthersExtension } from "@dynamic-labs/ethers-v5";
 
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <DynamicContextProvider
+//       settings={{
+//         // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+//         environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+//         walletConnectorExtensions: [EthersExtension],
+//         walletConnectors: [EthereumWalletConnectors],
+//       }}
+//     >
+//       <DynamicWidget />
+//     </DynamicContextProvider>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react'
+import styled from 'styled-components'
+
+import { useAppSelector } from './hooks'
+
+import RoomSelectionDialog from './components/RoomSelectionDialog'
+import LoginDialog from './components/LoginDialog'
+import ComputerDialog from './components/ComputerDialog'
+import WhiteboardDialog from './components/WhiteboardDialog'
+import VideoConnectionDialog from './components/VideoConnectionDialog'
+import Chat from './components/Chat'
+import HelperButtonGroup from './components/HelperButtonGroup'
+import MobileVirtualJoystick from './components/MobileVirtualJoystick'
+
+const Backdrop = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`
+
+function App() {
+  const loggedIn = useAppSelector((state) => state.user.loggedIn)
+  const computerDialogOpen = useAppSelector((state) => state.computer.computerDialogOpen)
+  const whiteboardDialogOpen = useAppSelector((state) => state.whiteboard.whiteboardDialogOpen)
+  const videoConnected = useAppSelector((state) => state.user.videoConnected)
+  const roomJoined = useAppSelector((state) => state.room.roomJoined)
+
+  let ui: JSX.Element
+  if (loggedIn) {
+    if (computerDialogOpen) {
+      /* Render ComputerDialog if user is using a computer. */
+      ui = <ComputerDialog />
+    } else if (whiteboardDialogOpen) {
+      /* Render WhiteboardDialog if user is using a whiteboard. */
+      ui = <WhiteboardDialog />
+    } else {
+      ui = (
+        /* Render Chat or VideoConnectionDialog if no dialogs are opened. */
+        <>
+          <Chat />
+          {/* Render VideoConnectionDialog if user is not connected to a webcam. */}
+          {!videoConnected && <VideoConnectionDialog />}
+          <MobileVirtualJoystick />
+        </>
+      )
+    }
+  } else if (roomJoined) {
+    /* Render LoginDialog if not logged in but selected a room. */
+    ui = <LoginDialog />
+  } else {
+    /* Render RoomSelectionDialog if yet selected a room. */
+    ui = <RoomSelectionDialog />
+  }
+
   return (
-    <DynamicContextProvider
-      settings={{
-        // Find your environment id at https://app.dynamic.xyz/dashboard/developer
-        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-        walletConnectorExtensions: [EthersExtension],
-        walletConnectors: [EthereumWalletConnectors],
-      }}
-    >
-      <DynamicWidget />
-    </DynamicContextProvider>
-  );
-};
+    <Backdrop>
+      {ui}
+      {/* Render HelperButtonGroup if no dialogs are opened. */}
+      {!computerDialogOpen && !whiteboardDialogOpen && <HelperButtonGroup />}
+    </Backdrop>
+  )
+}
 
-export default App;
+export default App
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,34 +1,42 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import { setup } from "./mud/setup";
-import { MUDProvider } from "./MUDContext";
-import mudConfig from "contracts/mud.config";
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@mui/material/styles'
 
-const rootElement = document.getElementById("react-root");
-if (!rootElement) throw new Error("React root not found");
-const root = ReactDOM.createRoot(rootElement);
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core'
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum'
 
-// TODO: figure out if we actually want this to be async or if we should render something else in the meantime
-setup().then(async (result) => {
-  root.render(
-    <MUDProvider value={result}>
-      <App />
-    </MUDProvider>
-  );
+import './index.scss'
+import './PhaserGame'
+import muiTheme from './MuiTheme'
+import App from './App'
+import store from './stores'
 
-  // https://vitejs.dev/guide/env-and-mode.html
-  if (import.meta.env.DEV) {
-    const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-    mountDevTools({
-      config: mudConfig,
-      publicClient: result.network.publicClient,
-      walletClient: result.network.walletClient,
-      latestBlock$: result.network.latestBlock$,
-      storedBlockLogs$: result.network.storedBlockLogs$,
-      worldAddress: result.network.worldContract.address,
-      worldAbi: result.network.worldContract.abi,
-      write$: result.network.write$,
-      recsWorld: result.network.world,
-    });
-  }
-});
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <DynamicContextProvider
+        settings={{
+          // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+          environmentId: '2762a57b-faa4-41ce-9f16-abff9300e2c9',
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <div style={{
+          position: 'fixed',
+          top: 15,
+          right: 15,
+        }}>
+          <DynamicWidget />
+        </div>
+
+        <ThemeProvider theme={muiTheme}>
+          <App />
+        </ThemeProvider>
+      </DynamicContextProvider>
+    </Provider>
+  </React.StrictMode>
+)
